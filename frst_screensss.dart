ca51463b07e90d095e9f328c0fa00cfb72f7dd30
Manisvkmr <manisvkmr@gmail.com>
2024-03-08 11:18:22 +0530

    first screen added

---
+++ frst_screensss.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:matcher/matcher.dart';
+
+
+class First extends StatelessWidget {
+  const First({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        height: 50,
+        width: 200,
+        color: Colors.cyan,
+      ),
+    );
+  }
+}
