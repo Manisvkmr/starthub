c23a4da683517a9c7048ebd0d2e997bbc0a52682
Manisvkmr <manisvkmr@gmail.com>
2024-03-08 11:33:09 +0530

    text added

--- git_2nd.dart
+++ git_2nd.dart
@@ -12,6 +12,7 @@ class Second extends StatelessWidget {
         height: 50,
           width: 100,
         color: Colors.cyanAccent,
+        child: const Text("mani",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,),),
       ),
     );
   }
